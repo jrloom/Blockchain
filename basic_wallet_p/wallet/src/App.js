4dc0e7ee3927aa68fdd88eab8423978c967de3e3
Jason Loomis <jason@jrloomis.com>
2020-04-10 02:58:11 -0400

    getting data from server

--- basic_wallet_p/wallet/src/App.js
+++ basic_wallet_p/wallet/src/App.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Form, DataCheck } from './components'
 
 function App() {
+  const [id, setID] = useState('0')
+  const [newID, setNewID] = useState('')
+  const [chain, setChain] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:5000/chain')
+      .then(({ data }) => {
+        setChain(data.chain)
+        console.log(data)
+      })
+      .catch((err) => console.log(err))
+  }, [id])
+
+  const handleChange = (e) => setNewID(e.target.value)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setID(newID)
+  }
+
   return (
     <div>
-      <div>Div!</div>
+      <Form submit={handleSubmit} change={handleChange} id={newID} />
+      <h1>Wallet {id}</h1>
+      <DataCheck data={chain} />
     </div>
   )
 }
